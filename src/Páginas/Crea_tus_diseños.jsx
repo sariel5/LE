15dc85b84bfd2d3afd3b99261a9ded0d6614e306
sariel5 <linel.rm.12@gmail.com>
2024-09-06 12:50:06 +0000

    ajustes adicionales

--- src/Páginas/Crea_tus_diseños.jsx
+++ src/Páginas/Crea_tus_diseños.jsx
@@ -1,21 +1,15 @@
-import React from 'react'
-import Menu_login from '../componentes/Menu_login'
-import img2 from '../Imagenes/img2.jpg'
+import React from "react";
+import Menu_login from "../componentes/Menu_login";
+import img2 from "../Imagenes/img2.jpg";
 
 function Crea_tus_diseños() {
   return (
-    <div className='Crea_tus_diseños'>
+    <div className="Crea_tus_diseños">
       <Menu_login />
-      <img src={img2} alt="img2" style={{ maxWidth: '30%' }}></img> 
+      <img src={img2} alt="img2" style={{ maxWidth: "30%" }}></img>
       <div class="card-body">
         <h5 class="card-title">Camisetas</h5>
       </div>
-      
-          
-        
-      
-      
-
     </div>
   );
 }
